ed32bdd86ab5954562be6ad87c39e7c7f2cbc8a1
Jinyao <jinyaox@uci.edu>
2023-11-08 18:08:44 -0800

    Incomplete Main

---
+++ main.py
@@ -0,0 +1,19 @@
+import generator
+import csv
+
+"""Main file
+    1. read inputs
+        load the information to the database 
+"""
+
+
+
+def read_and_load(file_path):
+    # while loop that takes user input and save it to a file 
+    # prompt when user enter exit then it exits
+    while(True):
+        
+
+def init_jet(file_path):
+    # read and load call 
+    # read the csv file and load to the global dict
